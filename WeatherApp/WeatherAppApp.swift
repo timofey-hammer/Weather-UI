5ee2db2e8f0a73b92815781b6ce64749510f9172
timofey hammer <timofey.hammer@gmail.com>
2023-04-18 16:07:52 +0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Тимофей Кубышин on 2023-04-18.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
